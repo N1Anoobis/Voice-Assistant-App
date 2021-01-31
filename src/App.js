a3c419da39f95b063d707224995e10deeb045155
N1Anoobis <mrbond84@hotmail.com>
2021-01-31 07:26:07 +0100

    Add recursion example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
 
 const App = () => {
-    return ( 
-        <div>
-            <h1>Alan IA News Applicaion</h1>
-        </div>
-     );
+
+  //  const person = {
+  //       firstName: 'John',
+  //       lastName: 'Doe',
+  //       address: {
+  //           street: 'North 1st street',
+  //           city: 'San Jose',
+  //           state: 'CA',
+  //           country: 'USA',
+  //           supprise:[1,2,3,4,5,6,]
+  //       }
+  //   }
+
+  //   const deepCopyFunction = (inObject) => {
+  //       let outObject, value, key
+
+  //       if (typeof inObject !== "object" || inObject === null) {
+  //         return inObject // Return the value if inObject is not an object
+  //       }
+
+  //       // Create an array or object to hold the values
+  //       outObject = Array.isArray(inObject) ? [] : {}
+
+  //       for (key in inObject) {
+  //         value = inObject[key]
+  //       //   console.log(key)
+  //         // Recursively (deep) copy for nested objects, including arrays
+  //         outObject[key] = deepCopyFunction(value)
+  //       }
+
+  //       return outObject
+  //     }
+
+
+  function digital_root(n) {
+    let reduced = []
+    let converted = []
+    let array = n.toString()
+    console.log(array, 'toString')
+    array = [...array]
+    console.log(array, 'split')
+    converted = array.map(n => n * 1)
+    console.log(converted, 'Number')
+    reduced = converted.reduce(
+      (a, c) => a + c,
+      0
+    );
+    console.log(reduced, 'reduce')
+    if (reduced > 9) {
+      digital_root(reduced)
+    }
+    if (reduced < 9) {
+      return reduced
+    }
+  }
+
+  digital_root(456)
+
+  return (
+    <div>
+    </div>
+  );
 }
- 
+
 export default App;
